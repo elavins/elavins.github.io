41df8204b1317f13b9b67dabd3d342ec49703302
elavins <58194791+elavins@users.noreply.github.com>
2021-02-20 15:27:14 -0500

    jk previous fix was wrong

--- practice_room/index.js
+++ practice_room/index.js
@@ -3,10 +3,10 @@
 //
 //
 
-import * as TONE from './node_modules/tone/build/Tone.js';
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as TONE from '../node_modules/tone/build/Tone.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
 
-import {GLTFLoader} from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 //---------------variables-----------------//
 
